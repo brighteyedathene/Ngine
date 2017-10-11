739c24a99c627e2de1c083b37d55a1d63153561a
Eoin O'Connor <eoconno5@itserv.scss.tcd.ie>
2017-10-11 17:40:43 +0100

    added more shaders, and some stupid shit

--- main.cpp
+++ main.cpp
@@ -12,9 +12,12 @@ static const char* vertexShaderSource = "\n\
 #version 450\n\
 layout (location = 0) in vec3 aPos;\n\
 \n\
+out vec4 vertexColor;\n\
+\n\
 void main()\n\
 {\n\
-	gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n\
+	gl_Position = vec4(aPos, 1.0f);\n\
+	vertexColor = vec4(0.5, 0.0, 0.0, 1.0);\n\
 }\n\
 ";
 
@@ -22,9 +25,32 @@ static const char* fragmentShaderSource = "\n\
 #version 450 core\n\
 out vec4 FragColor;\n\
 \n\
+in vec4 vertexColor;\n\
+\n\
 void main()\n\
 {\n\
-	FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f); \n\
+	FragColor = vertexColor; \n\
+}\n\
+";
+
+static const char* dummyFragmentShaderSource = "\n\
+#version 450 core\n\
+out vec4 FragColor;\n\
+uniform vec4 uniColor;\n\
+\n\
+void main()\n\
+{\n\
+	FragColor = uniColor; \n\
+}\n\
+";
+
+static const char* dummyVertexShaderSource = "\n\
+#version 450\n\
+layout (location = 0) in vec3 aPos;\n\
+\n\
+void main()\n\
+{\n\
+	gl_Position = vec4(aPos.x * 0.5f, aPos.y * 0.5f, aPos.z, 1.0f);\n\
 }\n\
 ";
 
@@ -135,20 +161,6 @@ int main(int argc, char** argv)
 	// initialise GLFWwindow and opengl rendering context
 	Initialise();
 
-#pragma region garbage
-
-	float vertices[] = {
-		-1.0f, -1.0f, 0.0f,
-		0.0f, -1.0f, 0.0f,
-		0.0f, 1.0f, 0.0f
-	};
-
-	// veery simple VBO creation
-	unsigned int VBO;
-	glGenBuffers(1, &VBO);
-	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
 	// compile shaders
 	unsigned int vertexShader = CompileShader(vertexShaderSource, GL_VERTEX_SHADER);
 	unsigned int fragmentShader = CompileShader(fragmentShaderSource, GL_FRAGMENT_SHADER);
@@ -158,11 +170,72 @@ int main(int argc, char** argv)
 	unsigned int shaderProgram = LinkShaders(shaders, 2);
 	glUseProgram(shaderProgram);
 
+	// TEST create another linked shader
+	unsigned int dummyVShader = CompileShader(dummyVertexShaderSource, GL_VERTEX_SHADER);
+	unsigned int dummyFShader = CompileShader(dummyFragmentShaderSource, GL_FRAGMENT_SHADER);
+	shaders[0] = dummyVShader;
+	shaders[1] =  dummyFShader;
+	unsigned int dummyShaderProgram = LinkShaders(shaders, 2);
+
+
 	// delete shaders (they're already in the program)
 	glDeleteShader(vertexShader);
 	glDeleteShader(fragmentShader);
 
-	// TODO vertex attribute pointers
+
+#pragma region garbage
+
+	// temporary data
+	float vertices[] = {
+		-1.0f, -1.0f, 0.0f,
+		1.0f, -1.0f, 0.0f,
+		0.0f, 1.0f, 0.0f,
+		1.0f, 1.0f, 0.0f
+	};
+	unsigned int indices[] = {
+		0, 1, 3, // first
+		3, 2, 0 // second
+	};
+
+	// very simple VAO creation
+	unsigned int VAO;
+	glGenVertexArrays(1, &VAO);
+
+	/* 
+	after this call, all VBO, EBO, glVertexAttribPointer 
+	and glEnableVertexAttribArray calls will be stored inside the bound VAO 
+	*/
+	glBindVertexArray(VAO);
+
+	// veery simple VBO creation
+	// fill the VBO with the stupid array of vertices
+	unsigned int VBO;
+	glGenBuffers(1, &VBO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	// very simple EBO
+	// fill the EBO with indices (of 'vertices' array) governing which trios of points make triangles
+	unsigned int EBO;
+	glGenBuffers(1, &EBO);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+	// link vertex attributes for VAO
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0); // defined as location 0 in the vertex shader
+
+	// testing some crap before render loop
+
+	// draw wire fram
+	//glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+
+	std::cout << "sizeof(vertices) = " << sizeof(vertices) << std::endl;
+	std::cout << "sizeof(vertices)/sizeof(vertices[0]) = " << sizeof(vertices) / sizeof(vertices[0]) << std::endl;
+
+	int n_attributes;
+	glGetIntegerv(GL_MAX_VERTEX_ATTRIBS, &n_attributes);
+	std::cout << "Maximum number of vertex attributes suppported: " << n_attributes << std::endl;
 
 #pragma endregion garbage
 
@@ -172,9 +245,28 @@ int main(int argc, char** argv)
 		// input
 		ProcessInput(window);
 
-		//rendering
+
+		// logic
+		float time = glfwGetTime();
+		float green = (sin(time) / 2.0f) + 0.5f;
+		int uniColor = glGetUniformLocation(dummyShaderProgram, "uniColor");
+		glUseProgram(dummyShaderProgram);
+		glUniform4f(uniColor, 0.0f, green, 0.0f, 1.0f);
+
+
+		// rendering
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
+
+		glUseProgram(shaderProgram);
+		glBindVertexArray(VAO);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+		glBindVertexArray(0); // unbind the vertex array
+
+		glUseProgram(dummyShaderProgram);
+		glBindVertexArray(VAO);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+		glBindVertexArray(0); // unbind the vertex array
 
 		// swap buffers, check events
 		glfwSwapBuffers(window);
