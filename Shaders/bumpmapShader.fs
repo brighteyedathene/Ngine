974660cdf0142ab2ba063084760ad78351fd48b8
Eoin O'Connor <eoconno5@itserv.scss.tcd.ie>
2018-02-08 13:46:25 +0000

    began parallax mapping - doesn't work yet

--- Shaders/bumpmapShader.fs
+++ Shaders/bumpmapShader.fs
@@ -11,11 +11,8 @@ in mat3 TBN;
 uniform samplerCube skybox;
 uniform vec3 viewPos;
 
-
 uniform sampler2D texturemap;
 uniform sampler2D normalmap;
-
-
 
 uniform vec3 refractiveIndexRGB;
 uniform float refractiveIndex;
@@ -23,26 +20,29 @@ uniform float bias;
 uniform float scale;
 uniform float power;
 
+vec2 GetParallaxOffset(vec2 texCoords, vec3 viewDir);
+
+float HEIGHT_SCALE = 3.0;
+
+
 void main()
 {
-	float ratio = 1/refractiveIndex;
-
+	vec3 I = normalize(FragPos - viewPos);
+	
+	//vec2 parallaxTexCoords = GetParallaxOffset(TexCoords, I);
+	vec2 parallaxTexCoords = TexCoords;
+	
 	// calculate normal from normal map and TBN
-	vec3 normalSample = texture(normalmap, TexCoords).rgb;
+	vec3 normalSample = texture(normalmap, parallaxTexCoords).rgb;
 	normalSample = normalize(normalSample * 2.0 - 1.0);
 	normalSample = normalize(TBN * normalSample);
-
+	
 	// Texture sample
-	vec3 textureSample = texture(texturemap, TexCoords).rbg;
+	vec3 textureSample = texture(texturemap, parallaxTexCoords).rbg;
 
 	// Reflection sample
-	vec3 I = normalize(FragPos - viewPos);
 	vec3 refl = reflect(I, normalSample);
 	vec3 reflectSample = texture(skybox, refl).rgb;
-
-
-
-
 
 	// Reflection Coefficient
 	float reflectionCoefficient = max(0, min(1, bias + scale * pow(1 + dot(I, normalSample), power) ));
@@ -53,4 +53,11 @@ void main()
 
 	FragColor = vec4(result, 1.0);
 
+}
+
+vec2 GetParallaxOffset(vec2 texCoords, vec3 viewDir)
+{
+	float height = texture(normalmap, texCoords).r;
+	vec2 p = viewDir.xy / viewDir.z * (height * HEIGHT_SCALE);
+	return texCoords - p;
 }
--- Shaders/bumpmapShader.vs
+++ Shaders/bumpmapShader.vs
@@ -17,22 +17,6 @@ uniform mat4 model;
 
 void main()
 {
-	// Calculating tangent, bitangent, and assemble TBN matrix
-	//vec3 tangent;
-	//vec3 cross1 = cross(aNormal, vec3(0.0, 0.0, 1.0));
-	//vec3 cross2 = cross(aNormal, vec3(0.0, 1.0, 0.0));
-	//if(length(cross1) > length(cross2))
-	//{
-	//	tangent = cross1;
-	//}
-	//else
-	//{
-	//	tangent = cross2;
-	//}
-	//tangent = normalize(tangent);
-	//vec3 bitangent = cross(aTangent, aNormal);
-
-
 	//TBN = mat3(aTangent, aBitangent, aNormal);
 	vec3 T = normalize(vec3(model * vec4(aTangent,   0.0)));
    	vec3 B = normalize(vec3(model * vec4(aBitangent, 0.0)));
@@ -40,10 +24,6 @@ void main()
    	TBN = mat3(T, B, N);
 
 	TexCoords = aTexCoord;
-	if (length(aTangent) < 0.1){
-		TexCoords = vec2(0.0, 0.0);
-	}
-
 	FragPos = vec3(model * vec4(aPos, 1.0));
 	Normal = normalize(mat3(transpose(inverse(model))) * aNormal);
 	gl_Position =  projectionview * model * vec4(aPos, 1.0);
